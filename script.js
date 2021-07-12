9161c6865cea9db20aa8d039993f2a334e194c46
Mimona Karki <kmimo7na@gmail.com>
2021-07-12 17:48:21 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+ removeStyle = (e) => {
+   if(e.propertyName != "transform") return;
+   e.target.classList.remove('playing');
+}
+ playSound = (event) =>{
+    const audioElement = document.querySelector(`audio[data-key= "${event.key}"]`);
+    const keyElement = document.querySelector(`.key[data-key= "${event.key}"]`);
+    if(!audioElement) return;
+    keyElement.classList.add('playing'); 
+    audioElement.currentTime = 0;
+    audioElement.play();
+}
+
+const keyElements = Array.from(document.querySelectorAll('.key'));
+keyElements.forEach(key => key.addEventListener('transitionend', removeStyle));
+window.addEventListener('keydown', playSound);
